0fbcccb5839a2ade61c5c4b15a130523a14711e7
Marchello <paskalovmarkus@gmail.com>
2025-03-10 17:03:59 +0200

    Create a bitcoin wallet; View all exchange rates

--- main.py
+++ main.py
@@ -54,6 +54,10 @@ def load_api_bitcoin(vs_currencies: str) -> dict[str, dict[str, int]]:
         print(e)
 
 def create_the_first_wallet(amount: str):
+    if not amount:
+        print('Empty string. Please, try again')
+        return
+    
     amount = float(amount)
 
     user_wallets.append({
@@ -62,6 +66,10 @@ def create_the_first_wallet(amount: str):
     })
 
 def create_a_wallet(currency: str, amount: str):
+    if not currency or not amount:
+        print('Empty string. Please, try again')
+        return
+    
     currency = currency.upper()
     amount = float(amount)
 
@@ -72,16 +80,45 @@ def create_a_wallet(currency: str, amount: str):
 
     load_api_exchange_rate(currency=currency)
 
+def create_a_bitcoin_wallet(select_wallet: str, amount: str):
+    if select_wallet == 'BTC':
+        print('Unable to use bitcoin wallet')
+        return
+    
+    if not amount or not select_wallet:
+        print('Empty string. Please, try again')
+        return
+    
+    select_wallet = select_wallet.upper()
+    amount = float(amount)
+
+    for wallet in user_wallets:
+        if wallet.get('currency') == select_wallet:
+            get_price_BTC = load_api_bitcoin(vs_currencies=select_wallet.lower())
+
+            if get_price_BTC is None:
+                print(f"Failed to fetch Bitcoin price for {select_wallet}. Try again later.")
+                return
+
+            if wallet['amount'] < amount:
+                print(f"Not enough funds in {select_wallet} wallet.")
+                return
+            
+            BTC_amount = amount / get_price_BTC
+
+            wallet['amount'] -= amount
+
+            user_wallets.append({
+                'currency': 'BTC',
+                'amount': BTC_amount
+            })
+
 username = input('Enter your username: ')
 amount = input('Please, deposit your first money: ')
 
 create_the_first_wallet(amount=amount)
 
 while True:
-    if not amount or not username:
-        print('Empty string. Please, try again')
-        break
-    
     print('')
     print(rf'{Fore.GREEN}$$\      $$\         $$\      $$\           $$\ $$\            $$\     {Style.RESET_ALL}')
     print(rf'{Fore.GREEN}$$$\    $$$ |        $$ | $\  $$ |          $$ |$$ |           $$ |    {Style.RESET_ALL}')
@@ -93,9 +130,9 @@ while True:
     print(rf'{Fore.GREEN}\__|     \__|        \__/     \__| \_______|\__|\__| \_______| \____/  {Style.RESET_ALL}')
     print(r'')
     print(r'')
-    print(f'Hi {username}! The main currency in the program: EUR; Exchange rates: {exchange_rates}')
+    print(f'Hi {username}! The main currency in the program: EUR')
     print('Please, select the option:')
-    print(f'1. Create a wallet\n2. Create a Bitcoin wallet\n3. Make a transfer to another wallet\n4. View all wallets\n5. Transaction history\n6. Delete all wallets\n7. Exit')
+    print(f'1. Create a wallet\n2. Create a Bitcoin wallet\n3. Make a transfer to another wallet\n4. View all wallets\n5. View all exchange rates\n6. Transaction history\n7. Delete all wallets\n8. Exit')
 
     option = input('Option: ')
 
@@ -105,6 +142,19 @@ while True:
 
         create_a_wallet(currency=currency, amount=amount)
 
+    if option == '2':
+        print('=' * 46)
+
+        for index, wallet in enumerate(user_wallets):
+                    print(f'{index + 1}. {wallet}')
+
+        print('=' * 46)
+
+        select_wallet = input('Please, select your wallet: ')
+        amount_from_card = input('Please, enter enter the amount: ')
+
+        create_a_bitcoin_wallet(select_wallet=select_wallet, amount=amount_from_card)
+
     if option == '4':
         print('=' * 46)
 
@@ -112,6 +162,14 @@ while True:
             print(f'{index + 1}. {wallet}')
 
         print('=' * 46)
+
+    if option == '5':
+        print('=' * 46)
+
+        for currency, item in exchange_rates.items():
+            print(f'{currency}: {item}')
+
+        print('=' * 46)
     
-    if option == '7':
+    if option == '8':
         break
